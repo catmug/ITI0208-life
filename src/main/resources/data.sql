9eed9593582f5d7a6ad1759e196703ee251d4a1d
Mikk <miraud@ttu.ee>
2019-02-18 16:29:21 +0200

    added test schema and dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-//todo
+INSERT INTO text_table (id, text, amount) VALUES (NEXT VALUE FOR seq1, 'cool', 23.23);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,9 @@
-//todo
+DROP SCHEMA public CASCADE;
+
+CREATE SEQUENCE seq1 START WITH 1;
+
+CREATE TABLE text_table (
+                          id BIGINT NOT NULL PRIMARY KEY,
+                          text VARCHAR(255) NULL,
+                          amount DECIMAL NOT NULL
+);
