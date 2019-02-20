2c2ecca28046f3e92d18ee8d53e6f522fe177224
Priit <prpall@ttu.ee>
2019-02-20 15:26:55 +0200

    initial dropdown commit

---
+++ web/src/components/CategoryDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <select>
+            tere
+            <option v-for="(option, name) in options" :value="option">{{ name }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "CategoryDropdown",
+        props: {
+            options: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
