4901902ec9fe76b041aa4459bb9009ffa22f5490
Mikk <miraud@ttu.ee>
2019-02-25 17:18:39 +0200

    started using lombok for models

--- src/main/java/ee/expensetracker/model/Expense.java
+++ src/main/java/ee/expensetracker/model/Expense.java
@@ -1,7 +1,14 @@
 package ee.expensetracker.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Expense {
 
     private Long expenseId;
@@ -10,53 +17,4 @@ public class Expense {
     private Timestamp insertTime;
     private String comment;
     private double amount;
-
-    Expense() {
-
-    }
-
-    public Expense(Long expenseId, Long category, Long user, Timestamp insertTime, String comment, double amount) {
-        this.expenseId = expenseId;
-        this.category = category;
-        this.user = user;
-        this.insertTime = insertTime;
-        this.comment = comment;
-        this.amount = amount;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public void setExpenseId(Long expenseId) {
-        this.expenseId = expenseId;
-    }
-
-    public Long getExpenseId() {
-        return expenseId;
-    }
-
-    public Long getCategory() {
-        return category;
-    }
-
-    public Long getUser() {
-        return user;
-    }
-
-    public Timestamp getInsertTime() {
-        return insertTime;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
 }
