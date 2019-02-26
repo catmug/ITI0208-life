043214fbaba10023d60f9e946836ea827724d0a8
wolfsbane4vw <kristavaabel@gmail.com>
2019-02-27 00:45:17 +0200

    got the selection id -> new category name from inpput -> returned object

--- web/src/components/RenameCategory.vue
+++ web/src/components/RenameCategory.vue
@@ -1,18 +1,51 @@
 <template>
     <div>
-        <CategoryDropdown></CategoryDropdown>
+        <p>Select category to rename:</p>
+        <CategoryDropdown @on-change="getSelectedCategory"></CategoryDropdown>
         <p></p>
-        <b-button variant="outline-primary">Edit</b-button>
+        <p>Rename category:</p>
+        <input v-model="category.name" type="text">
+        <p></p>
+        <b-button @click="editCategory" variant="outline-primary">Rename Category</b-button>
+        <p></p>
+        <p>{{message}}</p>
+
+
+
     </div>
 </template>
 
 <script>
 
     import CategoryDropdown from "./CategoryDropdown";
+    import axios from "axios"
 
     export default {
         name: "RenameCategory",
+        success: false,
+        data() {
+            return {
+                message: '',
+                category: {
+                    categoryId: 0,
+                    name: '',
+                    userId: 1
+                },
+
+            }
+        },
         components: {CategoryDropdown},
+        methods: {
+            editCategory() {
+                axios.post('http://localhost:8080/api/category/rename', this.category)
+                    .then(response => (this.success = true));
+                this.message = 'Category updated to ' + this.category.name
+            },
+
+            getSelectedCategory(e) {
+                this.category.categoryId = e;
+            }
+        },
     }
 </script>
 
