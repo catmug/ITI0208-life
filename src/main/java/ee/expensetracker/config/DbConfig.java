66e373e8f97e27863696b413e70a879453b5c0dd
Priit <prpall@ttu.ee>
2019-03-10 07:16:38 +0200

    added jpa dependency to build.gradle and added required methods to DbConfig

--- src/main/java/ee/expensetracker/config/DbConfig.java
+++ src/main/java/ee/expensetracker/config/DbConfig.java
@@ -1,5 +1,6 @@
 package ee.expensetracker.config;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,8 +12,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
@@ -43,4 +49,33 @@ public class DbConfig {
         return new JdbcTemplate(dataSource());
     }
 
+    @Bean
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory entityManagerFactory) {
+
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        factory.setPackagesToScan("model");
+        factory.setDataSource(dataSource());
+        factory.setJpaProperties(additionalProperties());
+        factory.afterPropertiesSet();
+
+        return factory.getObject();
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+        properties.setProperty("hibernate.dialect",
+                "org.hibernate.dialect.HSQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+
+        return properties;
+    }
 }
