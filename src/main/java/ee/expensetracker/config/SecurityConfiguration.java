16f6baa8683986f5947ada854c0fb290ae82ba8c
wolfsbane4vw <kristavaabel@gmail.com>
2019-03-20 12:23:58 +0200

    deleted unnecessary comments and annotations

--- src/main/java/ee/expensetracker/config/SecurityConfiguration.java
+++ src/main/java/ee/expensetracker/config/SecurityConfiguration.java
@@ -1,5 +1,4 @@
 package ee.expensetracker.config;
-import org.springframework.beans.factory.annotation.Autowired;;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser("test").password("{noop}test").roles("USER");
+                .withUser("test").password("{noop}best").roles("USER");
     }
 
     @Override
@@ -41,17 +40,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:9000", "http://localhost:8080"));
-        //configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.addAllowedOrigin("http://localhost:8080");
-        configuration.addAllowedOrigin("http://localhost:9000");
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:9000", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        //configuration.addAllowedHeader("Access-Control-Allow-Origin: http://localhost:8080");
-        //configuration.addAllowedHeader("*");
-        //configuration.addAllowedHeader("*");
-        /*configuration.addAllowedMethod("POST");
-        configuration.addAllowedMethod("DELETE");
-        configuration.addAllowedMethod("GET");*/
+        configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
