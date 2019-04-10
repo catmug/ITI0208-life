f6055b6e7e85f3ee335bcae5dfcfbd2b99e55ac5
Mikk <miraud@ttu.ee>
2019-04-10 12:51:52 +0300

    goals add proper month

--- web/src/components/AddGoal.vue
+++ web/src/components/AddGoal.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container" id="AddGoal">
         <div class="row">
+            <input type="number" v-model="goal.amount" aria-describedby="amountHelp"
+                   placeholder="Amount">
             <b-button @click="send()" variant="outline-primary">Add Goal</b-button>
             <p>{{message}}</p>
         </div>
@@ -34,8 +36,8 @@
         },
         methods: {
             send() {
-                axios.post('http://localhost:8080/api/goal', {month: 1, amount: 10})
-                    .then(response => (this.success = response.data.success));
+                axios.post('http://localhost:8080/api/goal', {month: 1, amount: this.goal.amount})
+                    .then(response => (this.success = response.data.success ));
             }
         }
     }
