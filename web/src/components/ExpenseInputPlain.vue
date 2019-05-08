f6f8ee8ee141c2c275b02f295060f7830ca016d1
wolfsbane4vw <kristavaabel@gmail.com>
2019-05-08 12:57:28 +0300

    user exists invalid feedback

--- web/src/components/ExpenseInputPlain.vue
+++ web/src/components/ExpenseInputPlain.vue
@@ -2,9 +2,16 @@
 
     <div>
         <div class="form-group">
-            <input type="number" v-model="expense.amount" aria-describedby="amountHelp" class="form-control"
-                   placeholder="Amount" data-cy="expense-amount" min="0">
+            <input type="number" v-validate="'required|min_value:0|numeric'" v-model="expense.amount"
+                   aria-describedby="amountHelp" class="form-control"
+                   placeholder="Amount" data-cy="expense-amount" min="0"
+                   name="amount"
+                   :class="{ 'is-invalid': submitted && errors.has('amount') }">
             <small id="amountHelp" class="form-text text-muted">How much you spent*</small>
+            <div
+                    v-if="submitted && errors.has('amount')"
+                    class="invalid-feedback"><p class="mb-0">{{ errors.first('amount') }}</p></div>
+
         </div>
         <div class="form-group">
 
@@ -16,7 +23,7 @@
             <CategoryDropdown @on-change="getSelectedCategory"></CategoryDropdown>
         </div>
 
-        <button class="btn btn-outline-primary float-right mb-4" data-cy="submit" @click="send">Save</button>
+        <button class="btn btn-outline-primary float-right mb-4" data-cy="submit" @click="validateForm">Save</button>
         <p class="float-left text-success" id="msg">{{message}}</p>
     </div>
 
@@ -31,6 +38,7 @@
         components: {CategoryDropdown},
         data: function () {
             return {
+                submitted: false,
                 all: {},
                 expense: {
                     amount: 0,
@@ -41,6 +49,14 @@
             }
         },
         methods: {
+            validateForm() {
+                this.submitted = true;
+                this.$validator.validateAll().then(valid => {
+                    if (valid) {
+                        this.send()
+                    }
+                });
+            },
             send() {
                 axios.post(process.env.VUE_APP_API + '/expense', this.expense
                 ).then(response => (this.success = response.data));
