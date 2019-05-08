51d2a4d0d57e416276ceab0bfadd61c4ffb2c2e3
Mikk <miraud@ttu.ee>
2019-05-08 10:33:36 +0300

    added goals to project

--- src/main/java/ee/expensetracker/controller/GoalController.java
+++ src/main/java/ee/expensetracker/controller/GoalController.java
@@ -1,25 +1,60 @@
 package ee.expensetracker.controller;
 
+import ee.expensetracker.dao.ExpenseDao;
 import ee.expensetracker.dao.UserDao;
+import ee.expensetracker.dto.ExpenseDto;
 import ee.expensetracker.dto.GoalDto;
+import ee.expensetracker.model.Expense;
 import ee.expensetracker.model.Goal;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class GoalController {
+    @Autowired
+    private ExpenseDao dao;
     @Autowired
     private UserDao userDao;
     @Autowired
     private ModelMapper modelMapper;
+
+
     @PostMapping("/goal")
     public void saveGoal(@RequestBody GoalDto goal) {
         System.out.println(goal);
         userDao.addGoal(modelMapper.map(goal, Goal.class));
     }
 
+    @GetMapping("/goal")
+    public Double getGoal() {
+        return userDao.getGoal();
+    }
+    @GetMapping("/goalleft")
+    public Double getGoalleft() {
+        List<ExpenseDto> expenseDtos = dao.findCategoriesByUserId().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
 
+        Double sum = Double.valueOf(0);
+
+        for (ExpenseDto e :
+                expenseDtos) {
+            sum += e.getAmount();
+        }
+
+        return sum;
+    }
+
+    private ExpenseDto convertToDto(Expense expense) {
+        ExpenseDto expenseDto = modelMapper.map(expense, ExpenseDto.class);
+        expenseDto.setInsertionDate(expense.getInsertTime());
+        return expenseDto;
+    }
 }
